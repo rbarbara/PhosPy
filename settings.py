e1aa672c4b10e2fe580c7712afe416dd4e27a4f2
Barbara Schlick <barbara.schlick@wh-netz.de>
2015-11-17 17:03:34 +0100

    settings.py erstellt damit djanog tut

---
+++ settings.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME' : os.path.join(BASE_DIR, 'steno.sqlite3'), 
+        'HOST' : '',
+        'USER' : '', 
+        'PASSWORD' : '', 
+        'PORT' : ''
+        }
+    }
+    
+SECRET_KEY = 'meinsecretkey'
+
+INSTALLED_APPS = (
+    'db',
+    )
