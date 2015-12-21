5cd11f8c764d8b62bb2478e6c5c3ee9973770040
Barbara Schlick <barbara.schlick@wh-netz.de>
2015-12-21 03:45:22 +0100

    Paar Default-Werte hinzugefügt und eine Ausgabe-Funktion

--- db/models.py
+++ db/models.py
@@ -15,12 +15,8 @@ class BasisModell(models.Model):
 class Klaeranlage(BasisModell):
     name = models.CharField(max_length = 100)
     ort = models.CharField(max_length = 50)
-
-
-
-
-
-
+    wert1 = models.CharField(max_length = 50, default = "nope")
+    wert2 = models.CharField(max_length = 50, default = "nope")
 
     # Funktion zum exportieren im CSV-Format
     def CSV_export(self, titelzeile = False):
