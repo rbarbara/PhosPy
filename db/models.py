176f2b6ee8efde4eac8a6fa310ca33550c525a30
rhabarberbarbara <barbara+rhabarberbarbara@selfnet.de>
2016-03-09 04:43:34 +0100

    DB entmüllt un mehrfache Zeitschritte pro Datensatz entfernt

--- db/models.py
+++ db/models.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-import Default_Werte as Initial
+
 
 # Basismodell abstract!
+# Damit jeder Datenbankeintrag ein Datum für zuletzt verändert und für erstellt enthält
 class BasisModell(models.Model):
     erstellt = models.DateTimeField(auto_now_add = True)
     zuletzt_geaendert = models.DateTimeField(auto_now = True)
@@ -12,65 +13,87 @@ class BasisModell(models.Model):
     class Meta:
         abstract = True
 
+
 # Klasse für den Ort der Kläranlage
 class Ort(models.Model):
     ort = models.CharField(max_length = 50, default = "")
 
-# Klasse für die Datenbankabbildung der Kläranlaged
+
+# Klasse für die Datenbankabbildung der Kläranlage
 class Klaeranlage(BasisModell):
     name = models.CharField(max_length = 100)
-    ort_ID = models.ForeignKey(Ort, default = 0)
+    ort = models.ForeignKey(Ort, default = 0)
     zuletzt_aktiv = models.BooleanField(default = False)
-    abwasserabgabe_phosphor = models.DecimalField(max_digits = 10, decimal_places = 9, default = 2.0)
-    kosten_schlammentsorgung = models.DecimalField(max_digits = 10, decimal_places = 9, default = 0.0)
-    #wert1 = models.CharField(max_length = 50, default = Initial.wert_1)
-    #wert2 = models.CharField(max_length = 50, default = "nope")
+    abwasserabgabe_p = models.DecimalField(max_digits = 10, decimal_places = 5, default = 11.93)
+    abwasserabgabe_n = models.DecimalField(max_digits = 10, decimal_places = 5, default = 7.158)
+    kosten_schlammentsorgung = models.DecimalField(max_digits = 10, decimal_places = 5, default = 0.0)
 
+
+# Probenahmestelle
 class Probenahmestelle(BasisModell):
     abkuerzung = models.CharField(max_length = 10, default = "")
     stelle = models.CharField(max_length = 50, default = "")
     hilfetext = models.TextField(max_length = 200, default = "")
 
-# Probe Zeitspanne
-class Probe_Zeitspanne(BasisModell):
+
+# Zeitspanne
+class Zeitspanne(BasisModell):
     zeitraum = models.CharField(max_length = 20, default = "")
+    zeitraum_pro = models.CharField(max_length = 20, default = "")
+
 
 # Probe flüssig
-class Probe(BasisModell):
-    klaeranlage_ID = models.ForeignKey(Klaeranlage)
-    probe_zeitspanne_ID = models.ForeignKey(Probe_Zeitspanne)
-    durchfluss = models.DecimalField(max_digits = 10, decimal_places = 9, default = 0.0)
-    p_ges = models.DecimalField(max_digits = 10, decimal_places = 9, default = 0.0)
-    n_ges = models.DecimalField(max_digits = 10, decimal_places = 9, default = 0.0)
+class Probe_fluessig(BasisModell):
+    klaeranlage = models.ForeignKey(Klaeranlage, default = 1)
+    #probe_zeitspanne_ID = models.ForeignKey(Zeitspanne, default = 1)
+    probe_probenahmestelle = models.ForeignKey(Probenahmestelle, default = 1)
+    durchfluss = models.DecimalField(max_digits = 13, decimal_places = 3, default = 0.0)
+    p_ges = models.DecimalField(max_digits = 13, decimal_places = 3, default = 0.0)
+    p_po4 = models.DecimalField(max_digits = 13, decimal_places = 3, default = 0.0)
     gerechnet = models.NullBooleanField(default = None)
 
+
 # Probe Asche/Schlamm
-class Probe_Asche(BasisModell):
-    klaeranlage_ID = models.ForeignKey(Klaeranlage)
-    probe_zeitspanne_ID = models.ForeignKey(Probe_Zeitspanne)
-    menge = models.DecimalField(max_digits = 10, decimal_places = 9, default = 0.0)
-    entsorgungskosten = models.DecimalField(max_digits = 10, decimal_places = 9, default = 0.0)
+class Probe_Schlamm_Asche(BasisModell):
+    klaeranlage = models.ForeignKey(Klaeranlage, default = 1)
+    #probe_zeitspanne_ID = models.ForeignKey(Zeitspanne, default = 1)
+    probe_probenahmestelle = models.ForeignKey(Probenahmestelle, default = 1)
+    menge = models.DecimalField(max_digits = 13, decimal_places = 3, default = 0.0)
+    p_ges_massengehalt = models.DecimalField(max_digits = 13, decimal_places = 3, default = 0.0)
+    gerechnet = models.NullBooleanField(default = None)
 
-# Verfahren
+
+# Verfahren, Abstrakte Klasse, nur als Vorlage für verschiedene Verfahren mit dem immer gleichen Grundgerüst!
 class Verfahren(BasisModell):
-    klaeranlage_ID = models.ForeignKey(Klaeranlage)
-    ansatzpunkt = models.ForeignKey(Probenahmestelle)
-    prozent_entnahme = models.DecimalField(max_digits = 10, decimal_places = 9)
-    investkosten = models.DecimalField(max_digits = 10, decimal_places = 9)
-    betriebskosten = models.DecimalField(max_digits = 10, decimal_places = 9) # Pro kg P
-    verkaufserkloes = models.DecimalField(max_digits = 10, decimal_places = 9) # Pro kg P
+    klaeranlage = models.ForeignKey(Klaeranlage)
+    #ansatzpunkt = models.ForeignKey(Probenahmestelle)
+    p_prozent_entnahme = models.DecimalField(max_digits = 5, decimal_places = 2)
+    investkosten = models.DecimalField(max_digits = 13, decimal_places = 3)
+    betriebskosten_pro_p = models.DecimalField(max_digits = 13, decimal_places = 3) # Pro kg P
+    verkaufserloes_pro_p = models.DecimalField(max_digits = 13, decimal_places = 3) # Pro kg P
+    zeitspanne_abschreibung = models.DecimalField(max_digits = 5, decimal_places = 2)
+    #auf_zeitspanne_angewendet = models.ForeignKey(Zeitspanne)
+
+    class Meta:
+        abstract = True
 
 
-    # Funktion zum exportieren im CSV-Format
-    def CSV_export(self, titelzeile = False):
-        pass
+# Verfahren welches am Ablauf der Kläranlage ansetzt
+class Verfahren_Ablauf(Verfahren):
+    pass
 
-    #Funtion zum importieren im CSV-Format
-    def CSV_import(self, titelzeile = False):
-        pass
 
-    def ausgabe(self):
-        print(self.id)
+# Verfahren welches beim Schlammwasser was zurück in die Biologie geht ansetzt
+class Verfahren_Schlammwasser(Verfahren):
+    n_nh4_vorher = models.DecimalField(max_digits = 13, decimal_places = 3)
+    n_nh4_nachher = models.DecimalField(max_digits = 13, decimal_places = 3)
 
-    def treeview_ausgabe(self):
-        return str(self.id) +" "+ self.name +" "+ self.ort +" "+ self.wert1 +" "+ self.wert2
+
+# Verfahren welches beim Faulschlamm ansetzt
+class Verfahren_Faulschlamm(Verfahren):
+    kosten_schlammentsorgung = models.DecimalField(max_digits = 13, decimal_places = 3)
+
+
+# Verfahren welches bei der Asche ansetzt
+class Verfahren_Asche(Verfahren):
+    kosten_schlammverbrennung = models.DecimalField(max_digits = 13, decimal_places = 3)
